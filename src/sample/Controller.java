5a3de7750da19606c1d09e348e7d290186a61863
manjenkomaksim <maksimma950@gmail.com>
2021-05-29 18:38:10 +0300

    Switching scenes and adding users to DB

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,16 @@
 package sample;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Controller {
 
@@ -15,19 +21,50 @@ public class Controller {
     private URL location;
 
     @FXML
-    private TextField loginField;
+    private TextField signInLoginField;
 
     @FXML
     private Button mainSignUpButton;
 
     @FXML
-    private TextField passwordField;
-
-    @FXML
     private Button mainSignInButton;
 
     @FXML
+    private PasswordField signInPasswordField;
+
+    @FXML
     void initialize() {
+
+        mainSignInButton.setOnAction(event -> {
+            String loginText = signInLoginField.getText().trim();
+            String passwordText = signInPasswordField.getText().trim();
+
+            if(!loginText.equals("") && !(passwordText.equals(""))){
+                loginUser(loginText, passwordText);
+            }
+            else
+                System.out.println("Field is empty");
+        });
+
+        mainSignUpButton.setOnAction(event -> {
+            mainSignUpButton.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("../UI/signUp.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setTitle("MyFuelStation");
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
+    }
+
+    private void loginUser(String loginText, String passwordText) {
 
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../UI/sample.fxml"));
         primaryStage.setTitle("MyFuelStation");
         primaryStage.setScene(new Scene(root, 840, 510));
         primaryStage.show();
