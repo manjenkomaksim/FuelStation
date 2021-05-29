7a1321608f83b7d27e2ae2bd5c7d418a60a9b6b6
manjenkomaksim <maksimma950@gmail.com>
2021-05-29 18:41:54 +0300

    Switching scenes and adding users to DB

---
+++ src/sample/Configs.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Configs {
+    protected String dbHost = "localhost";
+    protected String dbPort = "3306";
+    protected String dbUser = "root";
+    protected String dbPass = "17071977";
+    protected String dbName = "fuelstation";
+
+}
